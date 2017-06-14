232ea89b5e3de60a657f4b6b039fd1722bde27b5
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-06-14 17:41:52 +0200

    CORE-1169 CR fixes (part 2)
    Signed-off-by: Tamas Nyulas <tamas.nyulas@spryker.com>

---
+++ src/Spryker/Client/ProductSet/ProductSetClientInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\ProductSet;
+
+/**
+ * @method \Spryker\Client\ProductSet\ProductSetFactory getFactory()
+ */
+interface ProductSetClientInterface
+{
+
+    /**
+     * Specification:
+     * - Returns a list of Product Sets from Search.
+     * - The results are sorted by weight descending.
+     *
+     * @api
+     *
+     * @param int|null $limit
+     * @param int|null $offset
+     *
+     * @return array
+     */
+    public function getProductSetList($limit = null, $offset = null);
+
+}
